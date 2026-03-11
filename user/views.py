3dc3dadc48c9144ad17074dfbfa00f4c7c76e93e
gabarhdz <gabarhdz@outlook.com>
2026-03-11 08:44:07 -0600

    set view

--- user/views.py
+++ user/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from .serializers import ForumSerializer
 from .models import Forum
 # Create your views here.
+class AllForums(APIView):
+    def post(self,request,*args,**kwargs):
+        serializer = ForumSerializer(data = request.data, context={'request':request})
+        return ForumSerializer(Forum,context={'request':request}).data
