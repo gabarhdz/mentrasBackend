7afdeb567c886ffcf0a114d2bbc7ebeeb224c32e
gabarhdz <gabarhdz@outlook.com>
2026-03-13 00:24:15 -0600

    create URLs

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import AllForums
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('forums/',AllForums.as_view(),name='Get and Post forums'),
+    path('login/',TokenObtainPairView.as_view(),name='login'),
+    path('login/refresh/',TokenRefreshView.as_view(),name='refresh-login')
 ]
