0bf9f861f55509d680a70eab69677c5579f7130f
gabarhdz <gabarhdz@outlook.com>
2026-03-13 21:13:58 -0600

    views to create usres

--- user/views.py
+++ user/views.py
@@ -1,9 +1,36 @@
+from venv import logger
+
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import ForumSerializer
-from .models import Forum
+from .serializers import ForumSerializer,UserSerializer
+from .models import Forum,User
 # Create your views here.
+
+class AllUsers(APIView):
+    def get(self,request,*args,**kwargs):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True, context={'request': request})
+        return Response(serializer.data)
+    def post(self,request,*args,**kwargs):
+        try:
+            serializer = UserSerializer(data=request.data, context={'request': request})
+            
+            if serializer.is_valid():
+                user = serializer.save()
+                response_serializer = UserSerializer(user, context={'request': request})
+                return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+        except Exception as e:
+            logger.error(f"Error creating user: {str(e)}", exc_info=True)
+            return Response(
+                {'error': f'Error creating user: {str(e)}'}, 
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
 class AllForums(APIView):
     def get(self,request,*args,**kwargs):
         forums = Forum.objects.all()
