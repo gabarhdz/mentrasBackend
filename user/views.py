7347f39b649dafb6cbd739402cd5b7c82f42eb52
gabarhdz <gabarhdz@outlook.com>
2026-03-18 23:58:33 -0600

    setting permissions

--- user/views.py
+++ user/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import ForumSerializer,UserSerializer
 from .models import Forum,User
 # Create your views here.
 
 class AllUsers(APIView):
+    permission_classes=[AllowAny]
     def get(self,request,*args,**kwargs):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True, context={'request': request})
@@ -32,7 +34,7 @@ class AllUsers(APIView):
             )
 
 class UserDetail(APIView):
-
+    permission_classes = [IsAuthenticated]
     def get(self,request,id,*args,**kwargs):
         try: 
             user = User.objects.get(id=id)
