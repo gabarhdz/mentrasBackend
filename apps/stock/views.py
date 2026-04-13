c5f86b01eafc8a91573d80f2c302c0a0d0da2e1a
Gabrie Abarca <gabarhdz@outlook.com>
2026-04-13 15:02:34 -0600

    create permissions

--- apps/stock/views.py
+++ apps/stock/views.py
@@ -4,9 +4,18 @@ from rest_framework.response import Response
 from apps.stock.models import Item
 from apps.stock.serializers import ItemSerializer
 
+from globals.permissions import IsEmailVerified
 
 class AllItems(APIView):
+    permission_classes = [IsEmailVerified]
     def get(self, request):
         items = Item.objects.all()
         serializer = ItemSerializer(items, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            item = serializer.save()
+            return Response(ItemSerializer(item).data, status=201)
+        return Response(serializer.errors, status=400)
